7e5b08ba91d587cb19b3fc7f1976f77d0187bece
faiaz112 <jualam@ttu.edu>
2025-08-04 02:15:59 -0400

    D14

--- D14.py
+++ D14.py
@@ -1,12 +1,13 @@
 from data import data
 import random
+import os
 
 def compare(option1,option2):
     if option1['follower_count']>option2['follower_count']:
         return 'a'
     else:
         return 'b'
-
+first_round=True
 win_count=0
 a, b = random.sample(data, 2)
 print("""__  ___       __             
@@ -19,13 +20,22 @@ print("""__  ___       __
  / /___/ /_/ / |/ |/ /  __/ /    
 /_____/\____/|__/|__/\___/_/ """)
 
-print(f"Compare A: {a['name']}, a {a["description"]}, from {a['country']}")
-print(f"Compare B: {b['name']}, a {b["description"]}, from {b['country']}")
-choice= input("Who has more followers? Type 'A' or 'B': " ).lower()
-answer=compare(a,b)
-if choice==answer:
-    win_count+=1
-    print(f"You're right! Current score: {win_count}")
-else:
-    print(f"Sorry, that's wrong. Final score: {win_count}")
-    
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear') 
+    if not first_round:
+        print(f"Current score: {win_count}")
+    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
+    print(f"Compare B: {b['name']}, a {b['description']}, from {b['country']}")
+    choice= input("Who has more followers? Type 'A' or 'B': " ).lower()
+    answer=compare(a,b)
+    if choice==answer:
+        win_count+=1
+        print(f"You're right! Current score: {win_count}")
+        first_round=False
+        a=b
+        b=random.choice(data)
+        while b==a:
+            b=random.choice(data)
+    else:
+        print(f"Sorry, that's wrong. Final score: {win_count}")
+        break
